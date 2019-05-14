e70b55011e08f2587c12ed068f5cab1d70a54f07
Andrew Park <andrewpark93@hotmail.com>
2019-05-13 17:53:14 -0700

    "final updates"

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -1,49 +1,70 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ExampleWork from '../js/example-work';
+import {shallow} from 'enzyme';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
 
-import Enzyme from 'enzyme':
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 Enzyme.configure({ adapter: new Adapter() });
 
 const myWork = [
-    {
-        'title': "Work Example",
-        'image': {
-            'desc': "example screenshot of a project involving code",
-            'scr': "images/example1.png",
-            'comment': "",
-        }
-    },
-    {
-        'title': "Portfolio Boilerplate",
-        'image': {
-            'desc': "A Serverless Portfolio",
-            'scr': "images/example2.png",
-            'comment': "",
-        }
-    },
-    {
-        'title': "Work Example",
-        'image': {
-            'desc': "example screenshot of a project involving cats",
-            'scr': "images/example3.png",
-            'comment': "",
-
-        }
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
     }
+  },
+  {
+    'title': "Portfolio Boilerplate",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  }
 ];
 
 describe("ExampleWork component", () => {
-    let component = shallow<ExampleWork work={myWork}/>;
+  let component = shallow(<ExampleWork work={myWork}/>);
 
-    let images = component.find("img");
+  it("Should be a 'span' element", () => {
+    expect(component.type()).toEqual('span');
+  });
 
-    it("Should be a 'section' element", () => {
-        expect(images.length).toEqual(1);
-    });
+  it("Should contain as many children as work examples", () => {
+    expect(component.find('ExampleWorkBubble').length).toEqual(myWork.length);
+  });
 
-    it("Should contain as many children as there are work examples", () => {
-        expect(images.node.props.src).toEqual(myWork[1].image.src);
-    });
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  });
+});
+
+describe("ExampleWorkBubble Component", () => {
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}
+    openModal={mockOpenModalFn}/>);
+
+  let images = component.find("img");
+
+  it("Should contain a single img element", () => {
+    expect(images.length).toEqual(1);
+  });
+
+  it("Should have the img src set correctly", () => {
+    expect(images.prop('src')).toEqual(myWork[1].image.src);
+  });
+
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
+  });
+
 });
--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom")
+const myWork = [
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "lorem",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'scr': "images/example1.png",
+            'comment': "",
+        }
+    },
+    {
+        'title': "Portfolio Boilerplate",
+        'href': "https://example.com",
+        'desc': "lorem",
+        'image': {
+            'desc': "A Serverless Portfolio",
+            'scr': "images/example2.png",
+            'comment': "",
+        }
+    },
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "lorem",
+        'image': {
+            'desc': "example screenshot of a project involving cats",
+            'scr': "images/example3.png",
+            'comment': "",
 
-console.log("Webpack works")
+        }
+    }
+]
+
+ReactDom.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
