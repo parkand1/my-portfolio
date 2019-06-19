c1078d6da3a5bc03f5b7c768cad98ca0b8e059c2
Andrew Park <andrewpark93@hotmail.com>
2019-06-19 00:25:13 -0700

    updates to lambda debug

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,39 +1,38 @@
 import boto3
+from boto3.s3.transfer import TransferConfig
 import StringIO
 import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    sns = boto3.resource('sns', region_name='us-east-1')
-    topic = sns.Topic('arn:aws:sns:us-east-1:370032281567:deployPortfolioTopic:baa8b964-8b35-43df-83f4-4afd49a0d11c')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:370032281567:deployPortfolioTopic')
 
+    location = {
+        "bucketName" : 'portfolioandrewpark.info',
+        "objectKey" : "buildPortfolio.zip"
+    }
     try:
         job = event.get("CodePipeline.job")
 
-        location = {
-            'bucketName': 'portfolioandrewpark.info',
-            'objectKey': 'buildPortfolio.zip'
-        }
-
         if job:
-            for artifact in job['data']['inputArtifacts']:
-                if artifact['name'] == 'MyAppBuild':
-                    location = artifact['location']['s3Location']
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
 
         print "Building portfolio from " + str(location)
-
-        s3 = boto3.resource('s3')
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket('andrewparkportfolio.info')
-        build_bucket = s3.Bucket(location['bucketName'])
+        build_bucket = s3.Bucket(location["bucketName"])
 
-        buildPortfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location['objectKey'], buildPortfolio_zip)
+        buildBucket_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(["objectKey"], buildPortfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.upload_fileobj(obj, nm)
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
 
